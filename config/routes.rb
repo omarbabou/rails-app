f984ceff975aff868ec630e7a8468958d61fcf1c
omarbabou <obabou50@gmail.com>
2023-02-25 23:24:12 +0200

    Add changes required

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :categories do
     resources :transactions 
-
+  end
+  
   devise_scope :user do
     get 'users/sign_out' => 'devise/sessions#destroy'
   end
