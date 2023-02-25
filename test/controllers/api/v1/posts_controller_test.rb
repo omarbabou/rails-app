3585f06ba1c56df78fd5b3cb369e278b040d85a6
omarbabou <obabou50@gmail.com>
2023-02-25 02:14:57 +0200

    Implement user session, spash screen setup

--- test/controllers/api/v1/posts_controller_test.rb
+++ test/controllers/api/v1/posts_controller_test.rb
@@ -1,35 +1,37 @@
-require "test_helper"
+require 'test_helper'
 
 class Api::V1::PostsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @api_v1_post = api_v1_posts(:one)
   end
 
-  test "should get index" do
+  test 'should get index' do
     get api_v1_posts_url, as: :json
     assert_response :success
   end
 
-  test "should create api_v1_post" do
-    assert_difference("Api::V1::Post.count") do
-      post api_v1_posts_url, params: { api_v1_post: { content: @api_v1_post.content, title: @api_v1_post.title } }, as: :json
+  test 'should create api_v1_post' do
+    assert_difference('Api::V1::Post.count') do
+      post api_v1_posts_url, params: { api_v1_post: { content: @api_v1_post.content, title: @api_v1_post.title } },
+                             as: :json
     end
 
     assert_response :created
   end
 
-  test "should show api_v1_post" do
+  test 'should show api_v1_post' do
     get api_v1_post_url(@api_v1_post), as: :json
     assert_response :success
   end
 
-  test "should update api_v1_post" do
-    patch api_v1_post_url(@api_v1_post), params: { api_v1_post: { content: @api_v1_post.content, title: @api_v1_post.title } }, as: :json
+  test 'should update api_v1_post' do
+    patch api_v1_post_url(@api_v1_post),
+          params: { api_v1_post: { content: @api_v1_post.content, title: @api_v1_post.title } }, as: :json
     assert_response :success
   end
 
-  test "should destroy api_v1_post" do
-    assert_difference("Api::V1::Post.count", -1) do
+  test 'should destroy api_v1_post' do
+    assert_difference('Api::V1::Post.count', -1) do
       delete api_v1_post_url(@api_v1_post), as: :json
     end
 
